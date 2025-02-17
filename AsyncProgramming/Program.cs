5c7f50009cefcaff109bcae96b282c6cf4cdc987
Phone Thit Min Tun <phonethitmin.mm@gmail.com>
2025-02-17 17:51:26 +0700

    asynchronous, parallel programming

---
+++ AsyncProgramming/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    internal class Program
+    {
+        static  void Main(string[] args)
+        {
+
+            //await ShowOutput();
+            //await ShowOutput();
+            //Task t1 =ShowOutput();
+            //Task t2 =ShowOutput();
+            //Task t3 =ShowOutput();
+
+            //await Task.WhenAll(t1, t2, t3);
+            //Console.WriteLine("End");
+            Thread t1 = new Thread(()=>SayYes("abc"));
+            t1.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("x");
+            }
+            Console.WriteLine();
+        }
+        static void SayYes(string s)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("y");
+            }
+        }
+        public static async Task ShowOutput()
+        {
+
+            Console.WriteLine("Hello Async");
+            await Task.Delay(2000);
+            Console.WriteLine("Finish");
+        }
+
+    }
+}
---
+++ Batch5_day1/DapperExample.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch5_day1
+{
+    public  class DapperExample
+    {
+        private readonly string _connectionstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Batch5;User ID=admin;password=root123";
+        public void Read()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionstring))
+            {
+                string query = "SELECT * FROM tbl_blog where DeleteFlag = 0";
+                var lst = db.Query(query);
+            }
+        }
+    }
+}
